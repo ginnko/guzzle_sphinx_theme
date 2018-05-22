55e17cbee4d803f831c9c4a26ee1cee616bd699d
ginnko <yybnbn@126.com>
2018-05-22 23:54:06 +0800

    add new rule in gulp file
    1. add rule for adding new files to rebuild and refresh

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const shell = require('gulp-shell');
+const watch = require('gulp-watch');
+const batch = require('gulp-batch');
 
-gulp.task('browser-sync', [], () => {
+const dir = 'guzzle_sphinx_theme/guzzle_sphinx_theme';
+
+gulp.task('browser-sync', ['make-html'], () => {
   browserSync.init({
     server: {
       baseDir: "./demo/_build/html"
     }
   });
-  gulp.watch('./guzzle_sphinx_theme/guzzle_sphinx_theme/*.html', ['make-html']);
-  gulp.watch('./guzzle_sphinx_theme/guzzle_sphinx_theme/static/css/*.css', ['make-html']);
-  gulp.watch('./guzzle_sphinx_theme/guzzle_sphinx_theme/static/js/*.js', ['make-html']);
-  gulp.watch('./guzzle_sphinx_theme/guzzle_sphinx_theme', ['make-html']);
+  gulp.watch(dir + '/*.html', ['make-html']);
+  gulp.watch(dir + '/static/css/*.css', ['make-html']);
+  gulp.watch(dir + '/static/js/*.js', ['make-html']);
   gulp.watch("./demo/_build/html/*.html").on('change', browserSync.reload);
 });
 
@@ -19,4 +22,24 @@ gulp.task('make-html', shell.task([
   'cd demo && rm -rf _build && sphinx-build -b html -d _build/doctrees   . _build/html'
 ]));
 
-gulp.task('default', ['browser-sync']);
+
+gulp.task('new-html', function () {
+  watch(dir + '/*.html', batch(function (events, done) {
+      gulp.start('make-html', done);
+  }));
+});
+
+gulp.task('new-css', function () {
+  watch(dir + '/static/css/*.css', batch(function (events, done) {
+      gulp.start('make-html', done);
+  }));
+});
+
+gulp.task('new-js', function () {
+  watch(dir + '/static/js/*.js', batch(function (events, done) {
+      gulp.start('make-html', done);
+  }));
+});
+
+
+gulp.task('default', ['browser-sync', 'new-css', 'new-html', 'new-js']);
